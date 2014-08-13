326bd93022497c43da2c6adc69b3c47d10a4d55b
zhaooah <hao.zheng@tufts.edu>
2014-08-13 13:21:29 -0400

    With Map Version

---
+++ index.js
@@ -0,0 +1,112 @@
+$(document)
+  .ready(function() {
+
+    var
+      changeSides = function() {
+        $('.ui.shape')
+          .eq(0)
+            .shape('flip over')
+            .end()
+          .eq(1)
+            .shape('flip over')
+            .end()
+          .eq(2)
+            .shape('flip back')
+            .end()
+          .eq(3)
+            .shape('flip back')
+            .end()
+        ;
+      },
+      validationRules = {
+        firstName: {
+          identifier  : 'email',
+          rules: [
+            {
+              type   : 'empty',
+              prompt : 'Please enter an e-mail'
+            },
+            {
+              type   : 'email',
+              prompt : 'Please enter a valid e-mail'
+            }
+          ]
+        }
+      }
+    ;
+
+    $('.ui.dropdown')
+      .dropdown({
+        on: 'hover'
+      })
+    ;
+
+    $('.ui.form')
+      .form(validationRules, {
+        on: 'blur'
+      })
+    ;
+
+    $('.masthead .information')
+      .transition('scale in')
+    ;
+
+    setInterval(changeSides, 3000);
+
+  })
+;
+
+//Script for portfolio
+    window.open    = function(){};
+    window.print   = function(){};
+    // Support hover state for mobile.
+    if (false) {
+      window.ontouchstart = function(){};
+    }
+
+
+//Script for a herf transistion
+// Smooth scroll for in page links - http://wibblystuff.blogspot.in/2014/04/in-page-smooth-scroll-using-css3.html
+$(function(){
+    var target, scroll;
+
+    $("a[href*=#]:not([href=#])").on("click", function(e) {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            target = $(this.hash);
+            target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
+
+            if (target.length) {
+                if (typeof document.body.style.transitionProperty === 'string') {
+                    e.preventDefault();
+                  
+                    var avail = $(document).height() - $(window).height();
+    
+                    scroll = target.offset().top;
+                  
+                    if (scroll > avail) {
+                        scroll = avail;
+                    }
+
+                    $("html").css({
+                        "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
+                        "transition" : "1s ease-in-out"
+                    }).data("transitioning", true);
+                } else {
+                    $("html, body").animate({
+                        scrollTop: scroll
+                    }, 1000);
+                    return;
+                }
+            }
+        }
+    });
+
+    $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
+        if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
+            $(this).removeAttr("style").data("transitioning", false);
+            $("html, body").scrollTop(scroll);
+            return;
+        }
+    });
+});
+
